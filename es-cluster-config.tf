72084ecc7d2888afa849d2d01ede0e4aec8c5204
Georgi Georgiev <ggeorgiev@live.co.uk>
2015-08-25 06:02:59 +0100

    Terraform config and hide AWS access keys

---
+++ es-cluster-config.tf
@@ -0,0 +1,72 @@
+#Authenticate to AWS provider
+
+provider "aws" {
+    access_key = "AKIAJ-----"
+    secret_key = "kdVl9NybirYXOomr1Y+------------------"
+    region = "us-east-1"
+}
+
+#Create 3 EC2 Instances for ElasticSearch cluster nodes
+
+resource "aws_instance" "es-node-1" {
+    ami = "ami-d5c5d1e5"
+    instance_type = "m1.large"
+    tags {
+        Owner = "Georgi"
+	Environment = "Production"
+    }
+}
+
+resource "aws_instance" "es-node-2" {
+    ami = "ami-d5c5d1e5"
+    instance_type = "m1.large"
+    tags {
+        Owner = "Georgi"
+	Environment = "Production"
+    }
+}
+
+resource "aws_instance" "es-node-3" {
+    ami = "ami-d5c5d1e5"
+    instance_type = "m1.large"
+    tags {
+        Owner = "Georgi"
+	Environment = "Production"
+    }
+}
+
+# Create new load balancer for EC2 ElasticSearch Cluster
+
+resource "aws-elb" "lb-elasticsearch" {
+  name = "lb-es"
+  availability_zones = [
+    "us-east-1a",
+    "us-east-1a",
+    "us-east-1d"
+  ]
+
+  listener {
+    instance_port = 8000
+    instance_protocol = "http"
+    lb_port = 80
+    lb_protocol = "http"
+  }
+
+  health_check {
+    healthy_threshold = 2
+    unhealthy_threshold = 2
+    timeout = 3
+    target = "HTTP:8000/isALive.json"
+    interval = 30
+  }
+
+  instances = [
+    "${aws_instance.es-node-1.id}",
+    "${aws_instance.es-node-2.id}",
+    "${aws_instance.es-node-3.id}"
+  ]
+  cross_zone_load_balancing = true
+  idle_timeout = 400
+  connection_draining = true
+  connection_draining_timeout = 400
+}
